9e6baca46fba8f9badc8470d1cbb70900539119f
Swikriti Tripathi <swikriti808@gmail.com>
2024-06-11 14:48:48 +0545

    dockerize the app
    Signed-off-by: Swikriti Tripathi <swikriti808@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,17 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx()],
+  preview: {
+    port: 8080,
+    strictPort: true
+  },
+  server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: 'http://0.0.0.0:8080'
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
